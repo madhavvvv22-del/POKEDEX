85794995471d87469d795882d8384b66dfa877e5
madhavvvv22-del <madhavvvvsim22@gmail.com>
2026-08-15 07:54:42 +0530

    done with the link

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/home' element={<Pokedex />}/>
+      <Route path='/' element={<Pokedex />}/>
       <Route path='/pokedex' element={<Pokedex />}/>
       <Route path='/pokemon/:id' element={<PokemonDetails />}/>
 
